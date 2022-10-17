c7cad92f1f816d6b77fea82f8dbcc25cba48e7da
Jasper <daniel.j.cowley@hotmail.co.uk>
2022-10-17 21:44:14 +0100

    few improvments

--- modules/logging.js
+++ modules/logging.js
@@ -5,39 +5,10 @@ exports.logMessage = (message, logChannel) => {
         if(message.content == '' || message.content == undefined)
         {
             logChannel.send(codeBlock(`[${message.channel.name}] Attachment sent by  ${message.author.username}:`), attachments.first());
-            let attachmentsToSave = [];
-
-            attachments.forEach(attachment => {
-                attachmentsToSave.push({
-                    id: attachment.id,
-                    attachment: attachment.attachment,
-                    name: attachment.name,
-                    size: attachment.size,
-                    url: attachment.url,
-                    proxyURL: attachment.proxyURL,
-                    height: attachment.height,
-                    width: attachment.width
-                });
-            });
         }
         else
         {
             logChannel.send(codeBlock(`[${message.channel.name}] Message by ${message.author.username}: ${message.content}`), attachments.first());
-
-            let attachmentsToSave = [];
-
-            attachments.forEach(attachment => {
-                attachmentsToSave.push({
-                    id: attachment.id,
-                    attachment: attachment.attachment,
-                    name: attachment.name,
-                    size: attachment.size,
-                    url: attachment.url,
-                    proxyURL: attachment.proxyURL,
-                    height: attachment.height,
-                    width: attachment.width
-                });
-            });
         }
     }
     else
