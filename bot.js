751eeeccc17d8f5647dbbe19f3c85af3e037ed19
Jasper <daniel.j.cowley@hotmail.co.uk>
2022-10-25 16:33:21 +0100

    added dotenv support

--- bot.js
+++ bot.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config');
@@ -10,12 +11,12 @@ client.on('ready', () => {
 client.on('message', (message) => {
     if(message.author.bot) return;
     if(message.channel.type == 'dm') return;
-    if(message.guild.id !== config.server_id) return;
+    if(message.guild.id !== (process.env.SC_SERVER_ID ?? config.server_id)) return;
     // Message Logging Module
-    const log = client.channels.cache.find(channel => channel.id === config.logging_channel);
+    const log = client.channels.cache.find(channel => channel.id === (process.env.SC_LOGGING_CHANNEL ?? config.logging_channel));
     if(log !== undefined) logging.logMessage(message, log);
 
 });
 
 
-client.login(config.discord_token);
+client.login(process.env.SC_DISCORD_TOKEN ?? config.discord_token);
